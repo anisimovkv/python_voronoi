ef56267254d2e6be2dea1d77ffb4047202484b13
konstantin <konstantin.anisimov@eosda.com>
2020-03-07 14:35:17 +0200

    updated   - proccess in class voronoi by 07.03

--- src/procces.py
+++ src/procces.py
@@ -78,15 +78,74 @@ class Voronoi:
         if self.tree.is_leaf and event.y == self.tree.site.y:
             left = self.tree
             right = Arc(event.p)
-            start = Point(((self.first_point.x+self.event.p.x)/2,self.height))
+            start = Point(
+                ((self.first_point.x + self.event.p.x) / 2, self.height))
             edge = VoronoiEdge(start, self.first_point, event.p)
-            self.tree = Arc(edge = edge)
+            self.tree = Arc(edge=edge)
             self.tree.set_left(left)
             self.tree.set_right(right)
             self.edges.append(edge)
+            return
+        # Если лист представляет собой событие окружности, он больше
+        # не является корректным, поскольку разделяется
+        leaf = self.find_arc(event.p.x)
+        if leaf.circle_event:
+            leaf.circle_event.deleted = True
+        # Находим точку параболы, где event.pt.x разбивает ее пополам
+        # вертикальной линией
+        start = leaf.point_on_bisection_line(event.p.x, self.sweep_pt.y)
+
+        # Между двумя узлами находятся потенциальные ребра Вороного
+        neg_ray = VoronoiEdge(start, leaf.site, event.p)
+        pos_ray = VoronoiEdge(start, event.p, leaf.site)
+        self.edge.append(neg_ray)
+
+        # Изменение побережья новыми внутренними узалами
+        leaf.edge = pos_ray
+        leaf.is_leaf = False
+
+        left = Arc()
+        leaf.edge = neg_ray
+        leaf.set_left(Arc(leaf.site))
+        leaf.set_right(Arc(leaf.site))
+
+        # Проверка наличия потениального событие
+        # окружности слева или справа.
+        self.generate_circle_event(leaf.left)
+        self.generate_circle_event(leaf.right)
 
     def proccess_circle(self, event: Event):
-        pass
+        node = event.node
+
+        left_a = node.get_left_ancestor()
+        left = left_a.get_largest_descendant()
+        right_a = node.get_right_ancestor()
+        right = right_a.get_smallest_decendant()
+
+        if left.circle_event:
+            left.circle_event.deleted = True
+        if right.circle_event:
+            right.circle_event.deleted = True
+
+        p = node.point_on_bisection_line(event.p.x, self.sweep_point.y)
+        left_a.edge.end = p
+        right_a.edge.end = p
+
+        t = node
+        ancestor = None
+        while t != self.tree:
+            t = t.parent
+            if t == left_a:
+                ancestor = left_a
+            elif t == right_a:
+                ancestor = right_a
+        ancestor.edge = VoronoiEdge(p, left.site, right.site)
+        self.edges.append(ancestor.edge)
+
+        node.remove()
+
+        self.generate_circle_event(left)
+        self.generate_circle_event(right)
 
 
 class Arc:
